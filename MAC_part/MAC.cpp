5a23f2b7bdbc0a072392018d4132de20451ca0bd
Ksenia Burova <kburova@vols.utk.edu>
2016-10-27 22:54:12 -0400

    Improved code, needs debugging still

--- MAC_part/MAC.cpp
+++ MAC_part/MAC.cpp
@@ -1,3 +1,4 @@
+#include <iomanip>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -12,26 +13,64 @@
 using namespace std;
 
 const int KEY_SIZE = 32;
+//typedef std::basic_string<unsigned char> u_string;
 
 void processBlock(unsigned char * key, 
-				  unsigned char * block, 
-				  unsigned char * cipher){
+				  unsigned char * block){
+	u_string c;
+	c = (unsigned char *)encode(key, block).c_str();
 
-	cipher = (unsigned char *)encode(key, block).c_str();	
+	for (int i = 0; i < 16;i++){
+		block[i] = c[i];
+	}
 }
 
 void CBCGen(unsigned char * key, 
 			unsigned char * message,
 			int MES_SIZE,
-			unsigned char * IV){
+			unsigned char * IV,
+			unsigned char * tag){
 	
+	int i,j;
 	int lastBlockSize = MES_SIZE % 16;
 	int d = MES_SIZE/16;
+
 	unsigned char CurrentState[16];
-	cout << strlen((const char*)message)<<endl;
-	for (int i = 0; i < d; i++){
-		
+	for ( i = 0; i < 16; i++ ){
+		CurrentState[i] = IV[i];
 	}
+	
+	// XOR 4 first bytes of current state with message //
+	// size, or in other words, pad message size with  //
+	// 0's to block size and XOR with current state    //
+
+	for (i = 0; i < 4; i++){
+		CurrentState[i] ^= ((MES_SIZE >> i*8) & 0xff);
+	}
+	processBlock(key,CurrentState);
+
+	// for each block XOR Current state with message //
+	// and run result through Pseudorand F_k, output //
+	// new current state						     //
+
+	for ( i = 0; i < d; i++){
+		for ( j = 0; j < 16; j++){
+			CurrentState[j] ^= message[i*16 + j];
+		}
+		processBlock(key,CurrentState);
+	}
+
+	if (lastBlockSize != 0){
+		for ( j = 0; j < lastBlockSize; j++){
+			CurrentState[j] ^= message[i*16 + j];
+		}
+		processBlock(key,CurrentState);
+	}
+	
+	for ( i = 0; i < 16; i++ ){
+		tag[i]=CurrentState[i];
+	}
+	
 }
 
 bool HashMacVer(){
@@ -50,7 +89,8 @@ bool CBCVer(){
 
 int main(int argc, char** argv){
 
-	fstream M_file,K_file;
+	ifstream M_file,K_file;
+	ofstream T_file;
 	string k, m, temp;
 	int i,j, MES_SIZE;
 	int mode, task;
@@ -78,6 +118,12 @@ int main(int argc, char** argv){
 	MES_SIZE = m.size()/2;
 	unsigned char message[MES_SIZE];
 	
+	T_file.open(argv[3]);
+	if (T_file.fail()){
+		fprintf(stderr, "Couln't open file for tag -- %s\n", argv[3]);
+		exit(1);
+	}
+
 	// Convert hex strings to actual hex values // 
 	for (i = 0; i < KEY_SIZE; i++){
 		temp = k.substr(i*2,2);
@@ -98,9 +144,16 @@ int main(int argc, char** argv){
 	cout << "Type 1 Tag Generation or type 2 for for Verification : ";
 	cin >> task;
 
-	if (mode == 1 and task == 1)
-		CBCGen(key,message,MES_SIZE,IV);
-	else if ( mode == 1 and task == 2)
+	if (mode == 1 and task == 1){
+		unsigned char tag[16];
+		CBCGen(key,message,MES_SIZE,IV,tag);
+		
+		for (i = 0; i < 16; i++){
+			T_file << hex << setw(2) <<setfill('0') << tag[i];
+		}
+		T_file.close();
+
+	}else if ( mode == 1 and task == 2)
 		CBCVer();
 	else if ( mode == 2 and task == 1)
 		HashMacGen(key,message);
