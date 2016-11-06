9e756dc0d44338a427d5bbaaa4c3d763e5ac9382
Ksenia Burova <kburova@vols.utk.edu>
2016-11-05 20:44:53 -0400

    Fully working Mac generation/verification

--- MAC_part/aes.cpp
+++ MAC_part/aes.cpp
@@ -18,7 +18,7 @@ int aesEcb(u_string key, u_string clear_text)
 	//u_string key = (unsigned char *) "a_key_simple_key";
 	//u_string clear_text = (unsigned char *) "1234567890uvwxyz";
 	u_string secret_txt = (unsigned char *) "\x14\x2f" "\x7d\x9e" "\xad\x8c" "\x06\x82" "\x30\xe0" "\xf1\x65" "\xa5\x2f" "\xf7\x89";
-	//u_string secret_txt = (unsigned char*) " /)??!B?7R?V?)???a???,(yz<?h?"; 
+	//u_string secret_txt = (unsigned char*) " /)??!B?7R?V?)???a???,(yz<?h?";
 	std::cerr << "ENCODING: " << (encode(key, clear_text)==secret_txt ? "ok" : "FAIL") << std::endl;
 	std::cerr << "ENCODING: " << (char*)clear_text.c_str() << " --> " <<std::showbase<<std::hex<< (char*)encode(key, clear_text).c_str() << std::endl;
 	std::cerr << "DECODING: " << (decode(key, secret_txt)==clear_text ? "ok" : "FAIL") << std::endl;
@@ -35,8 +35,8 @@ static u_string encode(u_string key, u_string data)
 	EVP_CIPHER_CTX_set_padding(&ctx, false);
 	unsigned char buffer[1024], *pointer = buffer;
 	int outlen;
-//	std::cout << "key: "<<(char*)key.c_str()<<"\ndata: "<<(char*)data.c_str()<<std::endl; 
-//	std::cout << data.length() << std::endl;
+//	std::cout << "key: "<<(char*)key.c_str()<<"\ndata: "<<(char*)data.c_str()<<std::endl;
+	std::cout << data.length() << std::endl;
 	EVP_EncryptUpdate (&ctx, pointer, &outlen, data.c_str(), data.length()) or ABORT();
 	pointer += outlen;
 	EVP_EncryptFinal_ex(&ctx, pointer, &outlen) or ABORT();
