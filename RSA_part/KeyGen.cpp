7688d264cc15cab0995fdb8e25561d5eb20a604d
Ksenia Burova <kburova@vols.utk.edu>
2016-11-06 20:00:37 -0500

    Have KeyGen functioning, but Fermat Testing gives not primes sometimes
    * have to consult teacher to fix this or to improve at least

--- RSA_part/KeyGen.cpp
+++ RSA_part/KeyGen.cpp
@@ -5,61 +5,188 @@
 #include <fstream>
 #include <gmp.h>
 #include <ctime>
+#include <cmath>
+#include <openssl/bn.h>
 using namespace std;
-unsigned long mix(unsigned long a, unsigned long b, unsigned long c);
 
 void keyGen(string & PK_file, string &SK_file) {
-    fstream PK, SK;
-    unsigned long int n;
-    unsigned long seed =  mix(clock(), time(NULL), getpid());
-    mpz_t p, q, temp, exp;
-    mpz_inits(p,q,temp,exp);
+    bool notFound = true;
 
-    /*** Initialize state and seed for random function ***/
-    gmp_randstate_t state;
-    gmp_randinit_mt (state);
-    gmp_randseed_ui (state, seed);
+    FILE  *PK, *SK;
+    unsigned long int secParam, limit;
+    int i, j;
+    BIGNUM p,q, groupMem,gcd, one, rem;
+    BN_init(&p);
+    BN_init(&q);
+    BN_init(&groupMem);
+    BN_init(&gcd);
+    BN_init(&one);
+    BN_init(&rem);
+    BIGNUM *temp1 = BN_new();
+    BIGNUM *temp2 = BN_new();
+    BIGNUM *n = BN_new();
+    BIGNUM *e = BN_new();
+    BIGNUM *d = BN_new();
+    BIGNUM *n_minus1 = BN_new();
+    BIGNUM *N = BN_new();
+    BIGNUM *orderOfGroup = BN_new();
 
-//    PK.open(PK_file.c_str());
-//    if (PK.fail()){
-//        fprintf(stderr, "Couldn't open Public Key file %s\n", PK_file.c_str());
-//        exit(1);
-//    }
-//    SK.open(SK_file.c_str());
-//    if (SK.fail()){
-//        fprintf(stderr, "Couldn't open Secret Key file %s\n", SK_file.c_str());
-//        exit(1);
-//    }
+    BN_CTX *x = BN_CTX_new();
+    BN_one(&one);
 
-    cout << "Enter securitu parameter n: "<<endl;
-    cin >> n;
-    cout << "Generating p and q..."<<endl;
+    PK = fopen(PK_file.c_str(), "w");
+    if (PK == NULL) {
+        fprintf(stderr, "Couldn't open Public Key file %s\n", PK_file.c_str());
+        exit(1);
+    }
+
+    SK = fopen(SK_file.c_str(), "w");
+    if (SK == NULL) {
+        fprintf(stderr, "Couldn't open Secret Key file %s\n", SK_file.c_str()) ;
+        exit(1);
+    }
+
+    cout << "Enter security parameter n: "<<endl;
+    cin >> secParam;
+
+
+    cout << "Generating p and q..." << endl;
+
+    BN_set_word(n, secParam);
+    BN_set_word(n_minus1, secParam - 1);
+
     /*** Generating random n-bit primes p and q ***/
+    limit = 3 * pow(secParam, 2);
 
+    while(notFound) {
+        for (i = 0; i < limit; i++) {
+            BN_rand(&p, secParam, 0, 1);
+            /***      Check primality:
+             *   1. Choose uniform element from Group of order N-1
+             *   2. Check if really a group member by checking GCD of 1
+             *   3. Compute mod N for that number raised to the power of N-1
+             *   4. If it's 1, N is probably rime                                ***/
 
-    mpz_urandomb (p, state, n);
-    mpz_setbit (p, n-1);
-   // printf("Size: ");
-    //cout <<mpz_sizeinbase (p, 2)<<endl;
-   // mpz_powm_ui (exp, p, n-1 , n);
-    printf("p: %s\n", mpz_get_str (NULL, 10, p));
-    cout << endl;
+            while (true) {
+                BN_rand_range(&groupMem, &p);
+                if (BN_gcd(&gcd, &p, &groupMem, x) == 0) {
+                    fprintf(stderr, " Couldn't compute GCD for p and possible group member\n");
+                    exit(1);
+                }
+                /*** if gcd == 1 for rand element and N, then it's in a group ***/
+                if (BN_cmp(&one, &gcd) == 0) {
+                    break;
+                }
+            }
+            BN_mod_exp(&rem, &groupMem, n_minus1, n, x);
+            if (BN_cmp(&one, &rem) == 0) {
+                printf("Found possible prime p...\n");
+                break;
+            }
+        }
+        for (i = 0; i < limit; i++) {
+            BN_rand(&q, secParam, 0, 1);
+            /***      Check primality:
+             *   1. Choose uniform element from Group of order N-1
+             *   2. Check if really a group member by checking GCD of 1
+             *   3. Compute mod N for that number raised to the power of N-1
+             *   4. If it's 1, N is probably rime
+             *   5. Also check here of GCD for q and p is also 1 (Carmichael numbers MAY fail check) ***/
 
-}
+            while (true) {
+                BN_rand_range(&groupMem, &q);
+                if (BN_gcd(&gcd, &q, &groupMem, x) == 0) {
+                    fprintf(stderr, " Couldn't compute GCD for p and possible group member\n");
+                    exit(1);
+                }
+                /*** if gcd == 1 for rand element and N, then it's in a group ***/
+                if (BN_cmp(&one, &gcd) == 0) {
+                    break;
+                }
+            }
+            BN_mod_exp(&rem, &groupMem, n_minus1, n, x);
+            if (BN_cmp(&one, &rem) == 0 and BN_cmp(&p, &q) != 0) {
+                BN_gcd(&gcd, &q, &p, x);
+                if (BN_cmp(&one, &gcd) == 0) {
+                    printf("Found possible prime q...\n");
+                    break;
+                }
+            }
+        }
 
+        /***  calculate N = p x q, and the order of the group (p-1)(q-1) ***/
 
-/*** Found algorithm on stackoverflow.com ***/
-unsigned long mix(unsigned long a, unsigned long b, unsigned long c)
-{
-    a=a-b;  a=a-c;  a=a^(c >> 13);
-    b=b-c;  b=b-a;  b=b^(a << 8);
-    c=c-a;  c=c-b;  c=c^(b >> 13);
-    a=a-b;  a=a-c;  a=a^(c >> 12);
-    b=b-c;  b=b-a;  b=b^(a << 16);
-    c=c-a;  c=c-b;  c=c^(b >> 5);
-    a=a-b;  a=a-c;  a=a^(c >> 3);
-    b=b-c;  b=b-a;  b=b^(a << 10);
-    c=c-a;  c=c-b;  c=c^(b >> 15);
-    return c;
+        BN_mul(N, &p, &q, x);
+        BN_sub(temp1, &p, &one);
+        BN_sub(temp2, &q, &one);
+
+        BN_mul(orderOfGroup, temp1, temp2, x);
+
+        unsigned long int fermatPrime = 65537;
+        BN_set_word(e, fermatPrime);
+        if (BN_gcd(&gcd, e, orderOfGroup, x) == 0) {
+            fprintf(stderr, " Couldn't compute GCD for e and Order of the group\n");
+            exit(1);
+        }
+        if (BN_cmp(&one, &gcd) == 0) {
+            printf("e > 1 is chosen...\n");
+        }
+
+        if (BN_mod_inverse(d, e, orderOfGroup, x) != NULL) {
+            printf("d is calculated...\n");
+            notFound = false;
+        } else {
+            printf("Couldn't calculate d, restart process...\n");
+
+        }
+    }
+
+//    char *a = (char*)malloc(sizeof(char)*300);
+//    for (i= 0; i<300;i++) a[i] = '\0';
+//    a = BN_bn2dec(&p);
+//    printf ("p: %s\n", a);
+//
+//    char *b = (char*)malloc(sizeof(char)*300);
+//    for (i= 0; i<300;i++) b[i] = '\0';
+//    b = BN_bn2dec(&q);
+//    printf ("q: %s\n", b);
+//
+//    char *c = (char*)malloc(sizeof(char)*300);
+//    for (i= 0; i<300;i++) c[i] = '\0';
+//    c = BN_bn2dec(e);
+//    printf ("e: %s\n", c);
+//
+//    char *f = (char*)malloc(sizeof(char)*300);
+//    for (i= 0; i<300;i++) f[i] = '\0';
+//    f = BN_bn2dec(orderOfGroup);
+//    printf ("order of the Group: %s\n", f);
+//
+//    char *l = (char*)malloc(sizeof(char)*300);
+//    for (i= 0; i<30;i++) l[i] = '\0';
+//    l = BN_bn2dec(d);
+//    printf ("d: %s\n", l);
+//
+//    printf ("p bits: %d\n", BN_num_bits(&p) );
+//    printf ("q bits: %d\n", BN_num_bits(&q) );
+
+    printf("Writing Public Key to a file...\n");
+    BN_print_fp(PK, N);
+    BN_print_fp(PK, e);
+    printf("Writing Secret Key to a file...\n");
+    BN_print_fp(SK, N);
+    BN_print_fp(SK, d);
+
+    fclose(PK);
+    fclose(SK);
+
+    BN_CTX_free(x);
+    BN_free(n_minus1);
+    BN_free(n);
+    BN_free(N);
+    BN_free(e);
+    BN_free(d);
+    BN_free(temp1);
+    BN_free(temp2);
+
 }
 
