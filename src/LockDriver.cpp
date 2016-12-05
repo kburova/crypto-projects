47a315e378df6a3de30a171fa8dc13047c86008b
Ksenia Burova <kburova@vols.utk.edu>
2016-12-04 21:35:39 -0500

    Working on unlocking part

--- src/LockDriver.cpp
+++ src/LockDriver.cpp
@@ -8,34 +8,63 @@ int main(int argc, char** argv){
 
 
     string dir, PK, SK, SIG, userPK, userSIG, caPK,caSIG;
-   // ifstream in("/Users/kseniaburova/Documents/school/Fall2016/cs483/Prog2/input");
-    ifstream in("input2");
-    cin.rdbuf(in.rdbuf());
+    bool action;
 
-    cout << "Enter path for directory to lock" << endl;
-    cin >> dir;
-    cout << "Enter path for locking party Public Key" << endl;
-    cin >> PK;
-    cout << "Enter path for locking party Secret Key" << endl;
-    cin >> SK;
-    cout << "Enter path for locking party signature" << endl;
-    cin >> SIG;
+    cout << "Do you lock or unlock directory? Type 1 (lock) or 0 (unlock)" << endl;
+    cin >> action;
 
-    cout << "Enter path for user's Public Key" << endl;
-    cin >> userPK;
-    cout << "Enter path for user's signature" << endl;
-    cin >> userSIG;
+    if (action) {
+        ifstream in("/Users/kseniaburova/Documents/school/Fall2016/cs483/Prog2/input");
+        //ifstream in("input2");
+        cin.rdbuf(in.rdbuf());
 
-    cout << "Enter path for CA's Public Key" << endl;
-    cin >> caPK;
-    cout << "Enter path for CA's signature" << endl;
-    cin >> caSIG;
+        cout << "Enter path for directory to lock" << endl;
+        cin >> dir;
+        cout << "Enter path for locking party Public Key" << endl;
+        cin >> PK;
+        cout << "Enter path for locking party Secret Key" << endl;
+        cin >> SK;
+        cout << "Enter path for locking party signature" << endl;
+        cin >> SIG;
 
-    cout <<endl;
-    dirToLock dirObj(dir, PK, SK, SIG, userPK, userSIG, caPK, caSIG);
-    dirObj.verifyPKeys();
-    dirObj.generateAESKeys();
-    dirObj.encryptFiles();
+        cout << "Enter path for unlocking party Public Key" << endl;
+        cin >> userPK;
+        cout << "Enter path for unlocking party signature" << endl;
+        cin >> userSIG;
+
+        cout << "Enter path for CA's Public Key" << endl;
+        cin >> caPK;
+        cout << "Enter path for CA's signature" << endl;
+        cin >> caSIG;
+
+        cout << endl;
+        dirToLock dirObj(dir, PK, SK, SIG, userPK, userSIG, caPK, caSIG);
+        dirObj.verifyPKeys();
+        dirObj.generateAESKeys();
+        dirObj.encryptFiles();
+
+    }else{
+        ifstream in("/Users/kseniaburova/Documents/school/Fall2016/cs483/Prog2/unlockInput");
+        //ifstream in("unlockInput2");
+        cin.rdbuf(in.rdbuf());
+        cout << "Enter path for directory to unlock" << endl;
+        cin >> dir;
+        cout << "Enter path for unlocking party Public Key" << endl;
+        cin >> PK;
+        cout << "Enter path for unlocking party Secret Key" << endl;
+        cin >> SK;
+        cout << "Enter path for unlocking party signature" << endl;
+        cin >> SIG;
+
+        cout<<endl;
+        // cout << dir <<endl <<PK <<endl <<SK << endl << SIG <<endl;
+        dirToUnlock dirObj(dir, PK, SK, SIG);
+        dirObj.verifyPKeys();
+        dirObj.reportIdentity();
+        dirObj.verifyPKeys();
+        dirObj.verifySharedKeys();
+        dirObj.decryptKeysFile();
+    }
 
     return 0;
 }
